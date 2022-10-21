2fecb9331c7689766c1f119f47142701c2a0b629
ollip <olli.paq@outlook.de>
2022-10-21 16:07:21 +0200

    Another change

--- java-essentials-jdk/java-essentials-jdk-collections/src/main/java/org/paq/java/essentials/jdk/collections/model/Student.java
+++ java-essentials-jdk/java-essentials-jdk-collections/src/main/java/org/paq/java/essentials/jdk/collections/model/Student.java
@@ -3,26 +3,16 @@ package org.paq.java.essentials.jdk.collections.model;
 import java.time.LocalDate;
 
 public class Student {
-    private String firstName;
-    private String lastName;
-    private LocalDate birthDate;
+    private final String firstName;
+    private final String lastName;
+
+    //Stupid commit
+    private final LocalDate birthDate;
 
     public Student(String firstName, String lastName, LocalDate birthDate) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.birthDate = birthDate;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public LocalDate getBirthDate() {
-        return birthDate;
     }
 
     @Override
