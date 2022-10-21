68fc8d37fcf1b22e11787305b7530b3a71bb026a
cedricpaquet <107539896+cedricpaquet@users.noreply.github.com>
2022-10-21 22:19:30 +0800

    Changed firstName

--- java-essentials-jdk/java-essentials-jdk-collections/src/main/java/org/paq/java/essentials/jdk/collections/model/Student.java
+++ java-essentials-jdk/java-essentials-jdk-collections/src/main/java/org/paq/java/essentials/jdk/collections/model/Student.java
@@ -3,23 +3,23 @@ package org.paq.java.essentials.jdk.collections.model;
 import java.time.LocalDate;
 
 public class Student {
-    private final String firstName;
-    private final String lastName;
+    private final String firstName1;
+    private final String lastName2;
 
     //Stupid commit
     private final LocalDate birthDate;
 
-    public Student(String firstName, String lastName, LocalDate birthDate) {
-        this.firstName = firstName;
-        this.lastName = lastName;
+    public Student(String firstName, String lastName2, LocalDate birthDate) {
+        this.firstName1 = firstName;
+        this.lastName2 = lastName2;
         this.birthDate = birthDate;
     }
 
     @Override
     public String toString() {
         return "Student{" +
-                "firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
+                "firstName='" + firstName1 + '\'' +
+                ", lastName='" + lastName2 + '\'' +
                 ", birthDate=" + birthDate +
                 '}';
     }
